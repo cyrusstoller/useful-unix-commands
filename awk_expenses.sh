24487174164c4a3fd968e089cf6423d789d2460a
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-08-17 23:13:37 -0700

    Adding sample input and output to the awk_expenses file

--- awk_expenses.sh
+++ awk_expenses.sh
@@ -24,3 +24,193 @@ $2 == "CR"	{$1 = -$1}
 NF == 0		{sum = 0}			# Implicit Summation
 		{sum += $1}
 ' $1
+
+
+
+# Sample Input:
+#
+# # 	Jackson Hole Expenses
+# # 	    February, '81
+# # 
+# # Cal Diller ...
+# # 
+# # 71.25  Cowboy Steak House
+# # 24  M/M
+# # 26  Mexican
+# # 60  Lift Tickets
+# # 13.50  Pizza
+# # -------
+# # CAL
+# # 
+# # Bill Pooley ...
+# # 
+# # 48 * 3  Lift Tickets
+# # 14.04  Breakfast
+# # 368.16  Room
+# # 29.31  Drinks
+# # -------
+# # BILL
+# # 
+# # Ward Cunningham ...
+# # 
+# # 14.73  Gas
+# # 14.2
+# # 13.86
+# # 17
+# # 14.5
+# # 13.76
+# # -------
+# # GAS  Total Gas
+# # 
+# # 15  Blue Lion
+# # 48  Lift
+# # 30.55  Anthonys
+# # 69.24  Alpenhof
+# # GAS  Total Gas
+# # -------
+# # WARD
+# # 
+# # 
+# # Total Shared Expenses ...
+# # 
+# # CAL  Cal Diller
+# # BILL  Bill Pooley
+# # WARD  Ward Cunningham
+# # -------
+# # TOTAL / 3
+# # -------
+# # SHARE  Individual Share
+# # 
+# # 
+# # Individual Balances ...
+# # 
+# # BILL  Bill's Expenses
+# # SHARE DB
+# # -------
+# # SUM
+# # 3.86  Telephone, Cal
+# # 6.01  Telephone, Ward
+# # -------
+# # DB  Due Bill
+# # 
+# # 
+# # CAL  Cal's Expenses
+# # SHARE DB
+# # -------
+# # SUM
+# # 3.86 DB  Telephone
+# # -------
+# # DC  Due Cal
+# # 
+# # 
+# # WARD  Ward's Expenses
+# # SHARE DB
+# # -------
+# # SUM
+# # 6.01 DB  Telephone
+# # -------
+# # DW  Due Ward
+# # 
+# # 
+# # Doublechecking ...
+# # 
+# # DB
+# # DC
+# # DW
+# # -------
+# # ERROR  Unaccounted Funds
+
+# ------------------------------------------------
+
+# Sample Output:
+# 
+# # 	Jackson Hole Expenses
+# # 	    February, '81
+# # 
+# # Cal Diller ...
+# # 
+# #   71.25 Cowboy Steak House
+# #   24.00 M/M
+# #   26.00 Mexican
+# #   60.00 Lift Tickets
+# #   13.50 Pizza
+# # -------
+# #  194.75
+# # 
+# # Bill Pooley ...
+# # 
+# #   48.00 * 3 Lift Tickets
+# #   14.04 Breakfast
+# #  368.16 Room
+# #   29.31 Drinks
+# # -------
+# #  555.51
+# # 
+# # Ward Cunningham ...
+# # 
+# #   14.73 Gas
+# #   14.20
+# #   13.86
+# #   17.00
+# #   14.50
+# #   13.76
+# # -------
+# #   88.05 Total Gas
+# # 
+# #   15.00 Blue Lion
+# #   48.00 Lift
+# #   30.55 Anthonys
+# #   69.24 Alpenhof
+# #   88.05 Total Gas
+# # -------
+# #  250.84
+# # 
+# # 
+# # Total Shared Expenses ...
+# # 
+# #  194.75 Cal Diller
+# #  555.51 Bill Pooley
+# #  250.84 Ward Cunningham
+# # -------
+# # 1001.10 / 3
+# # -------
+# #  333.70 Individual Share
+# # 
+# # 
+# # Individual Balances ...
+# # 
+# #  555.51 Bill's Expenses
+# #  333.70 DB
+# # -------
+# #  221.81
+# #    3.86 Telephone, Cal
+# #    6.01 Telephone, Ward
+# # -------
+# #  231.68 Due Bill
+# # 
+# # 
+# #  194.75 Cal's Expenses
+# #  333.70 DB
+# # -------
+# # -138.95
+# #    3.86 DB Telephone
+# # -------
+# # -142.81 Due Cal
+# # 
+# # 
+# #  250.84 Ward's Expenses
+# #  333.70 DB
+# # -------
+# #  -82.86
+# #    6.01 DB Telephone
+# # -------
+# #  -88.87 Due Ward
+# # 
+# # 
+# # Doublechecking ...
+# # 
+# #  231.68
+# # -142.81
+# #  -88.87
+# # -------
+# #    0.00 Unaccounted Funds
